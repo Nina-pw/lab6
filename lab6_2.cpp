6505e3e2f48223998117e772381a677f7c2791c4
Pimmada <nina21551@gmail.com>
2022-12-09 16:25:35 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
+
+double deg2rad(double A){
+    return A*M_PI/180;
+}
+double rad2deg(double B){
+    return B*180/M_PI;
+}
+double findXComponent(double x1, double x2, double c1, double c2){
+    return x1*cos(c1)+x2*cos(c2);
+}
+double findYComponent(double y1, double y2, double d1, double d2){
+    return y1*sin(d1)+y2*sin(d2);
+}
+double pythagoras(double e1, double e2){
+    return sqrt(pow(e1,2)+pow(e2,2));
+}
+void showResult(double f1, double f2){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << "\n";
+    cout << "Length of the resultant vector = " << f1 << "\n";
+    cout << "Direction of the resultant vector (deg) = " << f2 << "\n";
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+}
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
